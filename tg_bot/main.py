fd56c183e1e6483643422272ce9324c53a0c3b11
Alexander <sashagaganov2000@gmail.com>
2023-06-12 18:39:40 +0300

    polling to wevhooks transition

--- tg_bot/main.py
+++ tg_bot/main.py
@@ -1,19 +1,41 @@
+from aiohttp.web import run_app
+from aiohttp.web_app import Application
+from aiogram.webhook.aiohttp_server import SimpleRequestHandler, setup_application
+
 from tg_bot import bot, dp
 from tg_bot.handlers import register_handlers, setup_commands
+from configs import STANDART_URL
 
 
-async def start():
-    print('Бот запущен')
-    await bot.delete_webhook(drop_pending_updates=True)
+
+async def on_startup():
+    await bot.set_webhook(f'{STANDART_URL}bot/mosgortrans', drop_pending_updates=True)
     await setup_commands(bot)
+    print('Бот запущен')
+
+async def on_shutdown():
+    await bot.delete_webhook(drop_pending_updates=True)
 
 
-async def main():
-    dp.startup.register(start)
+def main():
+    dp.startup.register(on_startup)
+    dp.shutdown.register(on_shutdown)
+
+    dp['base_url'] = STANDART_URL
 
     register_handlers(dp)
 
-    try:
-        await dp.start_polling(bot)
-    finally:
-        await bot.session.close()
+    app = Application()
+    app["bot"] = bot
+
+    SimpleRequestHandler(
+        dispatcher=dp,
+        bot=bot,
+    ).register(app, path="/bot/mosgortrans")
+    setup_application(app, dp, bot=bot)
+
+    run_app(app, host="127.0.0.1", port=8002)
+
+
+if __name__ == "__main__":
+    main()
